79dde275218cf3f89eb43e2c7efd1b4cdddbd2b0
William Lim <manvinedi@gmail.com>
2017-12-13 01:34:00 +0800

    update

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Documentation and Tutorials for React version 16.
 
+For most projects start development by running `npm start`
+
 https://reactjs.org/tutorial/tutorial.html
 
 https://reactjs.org/docs/hello-world.html
--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -3,18 +3,30 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 class Square extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: null
+    }
+  }
   render () {
     return (
-      <button className='square'>
-        {/* TODO */}
+      <button className='square' onClick={() => this.setState({value: 'X'})}>
+        {this.props.value}
       </button>
     )
   }
 }
 
 class Board extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
   renderSquare (i) {
-    return <Square />
+    return <Square value={i} />
   }
 
   render () {
